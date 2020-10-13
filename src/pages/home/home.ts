9af2fb4dfc9df85b9454a19633d9de5763369a3d
Alini Canedo <alini.canedo@gmail.com>
2020-10-12 21:33:44 -0300

    Obtendo os dados do formulário de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 @Component({
@@ -8,20 +9,27 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  //obj inicia vazio para fazer bidding com os campos do form
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
   // metodos padrões para desabilitar 
   // o menu se usuario n estiver "logado"
-
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
+
   login() {
+    console.log(this.creds)
     this.navCtrl.setRoot('CategoriasPage');
   }
 }
