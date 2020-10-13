f746338029b92f11ff9c54208a0396badc8cc1d6
Alini Canedo <alini.canedo@gmail.com>
2020-10-13 19:55:14 -0300

    Interceptor para incluir token nas requisições

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from './services/auth.service';
 import { StorageService } from './services/storage.service';
 import { ClienteService } from './services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -28,8 +29,8 @@ import { ClienteService } from './services/domain/cliente.service';
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    CategoriaService,AuthService, StorageService, ClienteService,
-    ErrorInterceptorProvider
+    CategoriaService, AuthInterceptorProvider, ErrorInterceptorProvider, AuthService, StorageService, ClienteService,
+
   ]
 })
 export class AppModule { }
